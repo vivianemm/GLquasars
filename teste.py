88a6c9d09de2967a33078affc70b2fca765fe25b
vivanemm <viviane.mmat@gmail.com>
2019-05-13 18:09:09 -0300

    1st version

--- teste.py
+++ teste.py
@@ -24,22 +24,111 @@ ORDER BY z
 # SpecObjId, z, ra, dec (deg)
 
 import pandas as pd
+import numpy as np
 from astropy.coordinates import SkyCoord
-from astropy import *
+from sklearn.cluster import DBSCAN
+import matplotlib.pyplot as plt
+import astropy.units as u
 
 
-# importando dados
+# importing data
 quasars = pd.read_csv("Quasars_SpecObjAll.csv", dtype={'SpecObjId': int})
-# print(quasars.head(5))
-
-#calculando distancia entre qsos de mesmo z
-
-# quasars.ra quasars.dec
-for i in quasars:
-    a = SkyCoord(i.ra, i.dec, unit = "deg")
+quasars_test = pd.read_csv("teste.csv", dtype={'SpecObjId': int})
 
 
+# determines the separation between quasars of indexes i and j
+def distance(data, i, j):
+    a = SkyCoord(data.loc[i, 'ra'], data.loc[i, 'dec'], unit="deg")
+    b = SkyCoord(data.loc[j, 'ra'], data.loc[j, 'dec'], unit="deg")
+    return a.separation(b).arcsecond
 
+
+# adds key and value to dictionary
+def add_dict(dict, key, value):
+    dict[key] = value
+
+
+# converts pandas dataframe into dictionary
+def pd_dict(data):
+    return data.to_dict()
+
+#dic = pd_dict(quasars_test)
+#print(dic)
+#print(dic['SpecObjId'][1])
+
+
+
+# creates a list a dictionaries with keys z, ids of quasars for that z
+# and separation between the pairs
+def sep_dictionaries(data):
+    dict_master = {}
+    seps = {}
+
+    for i in range(len(data)-1):
+        add_dict(seps, 'id' + str(i), data.loc[i, 'SpecObjId'])  # adds quasar i
+        cont = 1
+
+        while data.loc[i, 'z'] == data.loc[i+cont, 'z']:
+            sep = distance(data, i, i+cont)
+            add_dict(seps, str(i) + '_' + str(i+cont), sep) # adds separation
+            cont += 1
+
+        add_dict(dict_master, data.loc[i, 'z'], seps) # adds dictionary of distances to master dictionary
+
+    return dict_master
+
+def sep_dictionaries2(data): # usando pd to dict
+    dict_master = {}
+    seps = {}
+
+    for i in range(len(data)-1):
+        add_dict(seps, 'id' + str(i), data.loc[i, 'SpecObjId'])  # adds quasar i
+        cont = 1
+
+        while data.loc[i, 'z'] == data.loc[i+cont, 'z']:
+            sep = distance(data, i, i+cont)
+            add_dict(seps, str(i) + '_' + str(i+cont), sep) # adds separation
+            cont += 1
+
+        add_dict(dict_master, data.loc[i, 'z'], seps) # adds dictionary of distances to master dictionary
+
+    return dict_master
+
+
+
+def dbscan(X, eps, min_samples):
+    db = DBSCAN(eps, min_samples).fit(X)
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
+    labels = db.labels_
+
+    # Number of clusters in labels, ignoring noise if present.
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+    n_noise_ = list(labels).count(-1)
+
+    print('Estimated number of clusters: %d' % n_clusters_)
+    print('Estimated number of noise points: %d' % n_noise_)
+
+
+
+# Aitoff projection of quasars
+def aitoff(data):
+    c = SkyCoord(data.ra, data.dec, unit="deg")
+    ra_rad = c.ra.wrap_at(180 * u.deg).radian
+    dec_rad = c.dec.radian
+
+    plt.figure(figsize=(8, 4.2))
+    plt.subplot(111, projection="aitoff")
+    plt.title("Aitoff projection")
+    plt.grid(True)
+    plt.plot(ra_rad, dec_rad, 'o', markersize=1, alpha=0.3)
+    plt.show()
+    print(c)
+
+#result = sep_dictionaries(quasars_test)
+#print(result)
+
+# pairwise distances
 
 
 
--- testedoteste.py
+++ testedoteste.py
@@ -1,17 +1,46 @@
 from astropy.coordinates import SkyCoord
 import pandas as pd
-import numpy as np
-from csv import *
 
 
-#quasars = pd.read_csv("teste.csv", dtype={'SpecObjId': int})
+quasars = pd.read_csv("teste.csv", dtype={'SpecObjId': int})
 
 # criando listas com as valores de ra e dec
-#ras = quasars.ra
-#decs = quasars.dec
+ras = quasars.ra
+decs = quasars.dec
+z = quasars.z
+
+def distance(i, j):
+    a = SkyCoord(quasars.loc[i, 'ra'], quasars.loc[i, 'dec'], unit="deg")
+    b = SkyCoord(quasars.loc[j, 'ra'], quasars.loc[j, 'dec'], unit="deg")
+    return a.separation(b).arcsecond
+
+def add_dict(dict, key, value):
+    dict[key] = value
+
+
 
 # calculando distancias entre os quasares e adicionando a lista seps
-#seps = []
+sep_list = []
+
+cont = 1
+for i in range(0, len(quasars)):
+    seps = {}
+    add_dict(seps, 'z', quasars.loc[i, 'z'])  # adds group's z
+    add_dict(seps, 'id' + str(i), quasars.loc[i, 'SpecObjId']) # adds first quasar
+    #seps['id' + str(i)] = quasars.loc[i, 'SpecObjId']
+    while quasars.loc[i, 'z'] == quasars.loc[cont, 'z']:
+        print('ok')
+#    while z[i] == z[cont]:
+#        sep = distance(i, cont)
+#        add_dict(seps, 'id' + str(cont), sep)
+#        seps['id' + str(cont)] = quasars.loc[i, 'SpecObjId']
+        cont = cont + 1
+#
+ #   sep_list.append(seps)
+#print(sep_list)
+
+
+
 #for i in range(0,len(ras)):
 #    a = SkyCoord(ras[i], decs[i] , unit = "deg")
 #    for j in range(i+1,len(ras)):
@@ -19,25 +48,3 @@ from csv import *
 #        sep = a.separation(b).arcsecond
 #        if sep <= 5:
 #            seps.append(sep)
-
-def csv_dict_list(variables_file):
-    # Open variable-based csv, iterate over the rows and map values to a list of dictionaries containing key/value pairs
-
-    reader = csv.DictReader(open(variables_file, 'rb'))
-    dict_list = []
-    for line in reader:
-        dict_list.append(line)
-    return dict_list
-
-csv_dict_list("teste.csv")
-
-
-
-
-
-
-#    b = SkyCoord()
-#b = SkyCoord(23.631323, 30.353149 , unit = "deg")
-
-#sep = a.separation(b).arcsecond
-#print(sep)
