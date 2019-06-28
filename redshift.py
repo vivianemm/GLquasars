4f17ff1a12395d3d88ffc04c962aa87f16801e1d
vivanemm <viviane.mmat@gmail.com>
2019-06-28 20:20:52 -0300

    first

--- redshift.py
+++ redshift.py
@@ -15,18 +15,17 @@ def hist_z(data):
     correct_df = data[data.redshift != -9999.0]
     plt.style.use('seaborn')
     plt.figure()
-    plt.subplot(2, 1, 1)
-    z = correct_df.hist(column="redshift", bins=1000)
-    plt.subplot(2, 1, 2)
-    zerr = correct_df.hist(column="redshift_err", bins=1000)
 
-#    hist = np.histogram(correct_df["redshift"], bins=100)
- #   print(hist[1])
+    fig, axs = plt.subplots(2, 1)
+    correct_df[['redshift', 'redshift_err']].plot(ax=axs[0], kind='hist', bins=100, alpha=0.85)  # or 'kde'
+
+    correct_df.hist(ax=axs[1], column="redshift_err", bins=50, color='seagreen')
+
     plt.tight_layout()
     plt.savefig("test.png")
     return None
 
 
 hist_z(dataframe)
-
+hist_z()
 #end = time.ti
--- teste.py
+++ teste.py
@@ -35,6 +35,10 @@ from sklearn.cluster import DBSCAN
 from sklearn.metrics.pairwise import pairwise_distances
 import matplotlib.pyplot as plt
 import astropy.units as u
+import time
+from scipy.spatial.distance import squareform
+from scipy.spatial.distance import pdist
+
 
 
 # importing data
@@ -46,6 +50,13 @@ quasars_test2 = pd.read_csv("sdss_z_test.csv", dtype={'bestObjId': int})
 def distance(data, i, j):
     a = SkyCoord(data.loc[i, 'ra'], data.loc[i, 'dec'], unit="deg")
     b = SkyCoord(data.loc[j, 'ra'], data.loc[j, 'dec'], unit="deg")
+    return a.separation(b).arcsecond
+
+
+# determines the separation between objs u and v
+def metric_func2(u, v):
+    a = SkyCoord(u['ra'], u['dec'], unit="deg")
+    b = SkyCoord(v['ra'], v['dec'], unit="deg")
     return a.separation(b).arcsecond
 
 
@@ -81,30 +92,22 @@ def z_groups(dataf):
 
 
 
-def zgrous2(dataf):
-    dataf = dataf[dataf.redshift != -9999.0]
-    sorted_df = dataf.sort_values(by='redshift')
+#def zgroups2(dataf):
+#    dataf = dataf[dataf.redshift != -9999.0]
+#    sorted_df = dataf.sort_values(by='redshift')
 
     ###### remove NANs
-    df = sorted_df.dropna(subset=['redshift'])
+#    df = sorted_df.dropna(subset=['redshift'])
 
-    all_groups_df = df.loc[abs(df['redshift'] - df['redshift'][0]) <= 0.25]  # print(df['redshift'][0] ??????
+ #   all_groups_df = df.loc[abs(df['redshift'] - df['redshift'][0]) <= 0.25]  # print(df['redshift'][0] ??????
 
-    for row in dataf.itertuples(index=True, name='Pandas'):
-        group_df = df.loc[abs(df['redshift'] - getattr(row, 'redshift')) <= 0.25]
-        #group_df['center'] = getattr(row, 'objID')
-        pd.concat([all_groups_df, group_df], names=['', getattr(row, 'objID')])
-
-
-    print(all_groups_df)
-
-zgrous2(quasars_test2)
-
-
-
-
-
-
+#    for row in dataf.itertuples(index=True, name='Pandas'):
+#        group_df = df.loc[abs(df['redshift'] - getattr(row, 'redshift')) <= 0.25]
+#        group_df['center'] = getattr(row, 'objID')
+#        #pd.concat([all_groups_df, group_df], names=['', getattr(row, 'objID')])
+#        pd.concat([all_groups_df, group_df]
+#
+#    return all_groups_df
 
 
 
@@ -119,6 +122,17 @@ def metric_func(array1, array2):
     return a.separation(b).arcsecond
 
 
+def metric_func3(u, v):
+
+    cos = np.cos(90-u[1])*np.cos(90-v[1]) + np.sin(90-u[1])*np.sin(90-v[1])*np.cos(u[0]-v[0])
+    sep = np.arccos(cos)
+
+    #a = SkyCoord(u[0], u[1], unit="deg")
+    #b = SkyCoord(v[0], v[1], unit="deg")
+    #return a.separation(b).arcsecond
+    return sep
+
+
 # creates the distance matrix
 def D_matrix(dataf, f_metric):
     groups = z_groups(dataf)
@@ -129,6 +143,89 @@ def D_matrix(dataf, f_metric):
         all_matrices.append(d_matrix)  # adds group's matrix to list of matrices
     d_matrices = np.array(all_matrices)
     return d_matrices  # array with all matrices
+
+
+def D_matrix2(dataf, f_metric):
+
+    dataf = dataf[dataf.redshift != -9999.0]
+    #sorted_df = dataf.sort_values(by='redshift')
+
+    ###### remove NANs
+    df = dataf.dropna(subset=['redshift'])
+    t = 1
+    d=1
+    all_matrices = []
+    for row in df.itertuples(index=True, name='Pandas'):
+        group_df = df.loc[abs(df['redshift'] - getattr(row, 'redshift')) <= 0.25]  # z group with center i
+        print('group' + str(t))
+        t += 1
+        print(str(group_df.shape))
+
+
+        d_matrix = pairwise_distances(group_df[['ra', 'dec']].values, metric=f_metric)  # distance matrix for each z group
+        print('d matrix' + str(d))
+        d += 1
+        all_matrices.append(d_matrix)  # adds group's matrix to list of matrices
+
+    d_matrices = np.array(all_matrices)
+    return d_matrices  # array with all matrices
+
+
+def D_matrix3(dataf, f_metric):
+
+    dataf = dataf[dataf.redshift != -9999.0]
+    #sorted_df = dataf.sort_values(by='redshift')
+
+    ###### remove NANs
+    df = dataf.dropna(subset=['redshift'])
+    t = 1
+    d=1
+    all_matrices = []
+    for row in df.itertuples(index=True, name='Pandas'):
+        group_df = df.loc[abs(df['redshift'] - getattr(row, 'redshift')) <= 0.25]  # z group with center i
+        print('group' + str(t))
+        t += 1
+        print(str(group_df.shape))
+
+        d_array = pdist(group_df[['ra', 'dec']], f_metric)
+        d_matrix = squareform(d_array)
+
+        print('d matrix' + str(d))
+        d += 1
+        all_matrices.append(d_matrix)  # adds group's matrix to list of matrices
+
+    d_matrices = np.array(all_matrices)
+    return d_matrices  # array with all matrices
+
+
+def D_matrix3(dataf, f_metric):
+
+    dataf = dataf[dataf.redshift != -9999.0]
+    sorted_df = dataf.sort_values(by='redshift')
+
+    ###### remove NANs
+    df = sorted_df.dropna(subset=['redshift'])
+
+    all_matrices = []
+    for row in df.itertuples(index=True, name='Pandas'):
+        for row2 in df.itertuples():
+            if abs(getattr(row, 'redshift') - getattr(row2, 'redshift'))<=abs(getattr(row, 'redshift_err') + getattr(row2, 'redshift_err')):
+                pass
+            else:
+                sorted_df = sorted_df[:]
+
+
+        d_array = pdist(group_df[['ra', 'dec']], f_metric)
+        d_matrix = squareform(d_array)
+
+        print('d matrix' + str(d))
+        d += 1
+        all_matrices.append(d_matrix)  # adds group's matrix to list of matrices
+
+    d_matrices = np.array(all_matrices)
+    return d_matrices  # array with all matrices
+
+
 
 
 # applies dbscan to the distance matrix
@@ -146,9 +243,20 @@ def dbscan(d_matrix, eps, min_samples):
     print('Estimated number of noise points: %d' % n_noise_)
 
 
-#a = D_matrix(quasars_test, metric_func)
-#for x in a:
-#    dbscan(x, 5, 3)
+start = time.time()
+
+a = D_matrix3(quasars_test2, metric_func3)
+cont = 0
+for x in a:
+    dbscan(x, 5, 3)
+    print(cont)
+    cont+=1
+
+end = time.time()
+time = end - start
+print(time)
+
+
 
 
 # Aitoff projection of quasars
